d72f19b1dee6683adc3ad6349bcaffa33d4c7404
Tycho Scholtze <klokhuis2@tycho-s.nl>
2018-01-03 20:50:29 +0100

    Day 1
    All changest made in day 1, not mutch because I dint spend allot of time on it. Also: im really tired.

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int LoadFile(char* filePath, char* output);
+
+int main(int argCount, char** args)
 {
-    printf("Hello world!\n");
+    char* fileString;
+    //if(argCount == 2) LoadFile(args[1], fileString); this method is crashing the code. AKA im stupid.
+
+    return 0;
+}
+
+int LoadFile(char* filePath, char* output)
+{
+    FILE* file;
+    file = fopen(filePath, "r");
+    if(!file) return -1;
+
+    while(fgets(output, 1000, filePath) != NULL)
+    {
+    }
+
+    fclose(file);
     return 0;
 }
